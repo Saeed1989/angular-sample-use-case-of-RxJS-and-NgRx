0b9795f0d98f19ff929e6fc40ccf1a9fb176bef9
saeed1 <saeed.sharman@gmail.com>
2021-11-21 19:42:34 +0900

    adds custom error messages

--- src/app/players/services/player.service.ts
+++ src/app/players/services/player.service.ts
@@ -4,6 +4,7 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, tap, map } from 'rxjs/operators';
 import { Player } from '../../core/modles/player';
 import { NetworkService } from 'src/app/core/services/network.service';
+import { ConvertUtil } from 'src/app/core/utils/convert-util';
 
 @Injectable({
   providedIn: 'root',
@@ -49,16 +50,7 @@ export class PlayerService {
   private handleError(err: any) {
     // in a real world app, we may send the server to some remote logging infrastructure
     // instead of just logging it to the console
-    let errorMessage: string;
-    if (err.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      errorMessage = `An error occurred: ${err.error.message}`;
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
-    }
-    console.error(err);
+    let errorMessage = ConvertUtil.errorToErrorMsg(err);
     return throwError(errorMessage);
   }
 }
